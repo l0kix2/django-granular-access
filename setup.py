8cf5725ecd28ec7b157d804d6c8af67afdaa2168
Kirill Sibirev <l0kix2@gmail.com>
2012-11-17 19:43:10 +0400

    Fixed packages list in setup.py.

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
 import os
-from setuptools import setup
+from distutils.core import setup
 
 
-PACKAGE_NAME = 'django-granular-access'
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 README_FILEPATH = os.path.join(CURRENT_DIR, 'README.rst')
-REQUIREMENTS_FILEPATH = os.path.join(CURRENT_DIR, 'requirements.txt')
-
-
-def get_requirements():
-    with open(REQUIREMENTS_FILEPATH) as fp:
-        return fp.read().splitlines()
 
 
 setup(
-    name=PACKAGE_NAME,
-    version=0.1,
-    packages=['granular_access'],
-
+    name='django-granular-access',
+    version='0.1',
+    packages=[
+        'granular_access',
+        'granular_access.tests',
+        'granular_access.migrations'
+    ],
     author='Kirill Sibirev',
     author_email='k.sibirev@gmail.com',
     url='https://github.com/l0kix2/django-granular-access',
     description='Flexible permission system for Django.',
     long_description=open(README_FILEPATH).read(),
-    keywords='django permissions',
-    install_requires=get_requirements(),
+    keywords=['django', 'permissions'],
     license='MIT',
     classifiers=[
         'Environment :: Web Environment',
